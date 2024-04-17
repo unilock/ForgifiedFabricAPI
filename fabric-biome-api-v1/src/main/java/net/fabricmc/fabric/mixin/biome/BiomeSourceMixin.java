dc36698e26c4d22c4e837611d4166b83fbe6adb5
Su5eD <su5ed@outlook.com>
2024-04-17 16:07:32 +0200

    Fix mixin conflict with End's Phantasm
    Fixes #118

--- fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/BiomeSourceMixin.java
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/BiomeSourceMixin.java
@@ -17,11 +17,10 @@
 package net.fabricmc.fabric.mixin.biome;
 
 import java.util.Set;
-import java.util.function.Supplier;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.biome.Biome;
@@ -29,9 +28,9 @@ import net.minecraft.world.biome.source.BiomeSource;
 
 @Mixin(BiomeSource.class)
 public class BiomeSourceMixin {
-	@Redirect(method = "getBiomes", at = @At(value = "INVOKE", target = "Ljava/util/function/Supplier;get()Ljava/lang/Object;"))
-	private Object getBiomes(Supplier<Set<RegistryEntry<Biome>>> instance) {
-		return fabric_modifyBiomeSet(instance.get());
+	@ModifyReturnValue(method = "getBiomes", at = @At("RETURN"))
+	private Set<RegistryEntry<Biome>> getBiomes(Set<RegistryEntry<Biome>> instance) {
+		return fabric_modifyBiomeSet(instance);
 	}
 
 	protected Set<RegistryEntry<Biome>> fabric_modifyBiomeSet(Set<RegistryEntry<Biome>> biomes) {
