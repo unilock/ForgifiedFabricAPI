ba60825efe6a178ab1d86c621c1b0aa17dce6aa4
Su5eD <su5ed@outlook.com>
2024-09-22 13:54:12 +0200

    Fix crash with HNN
    Fixes #153
    Fixes Shadows-of-Fire/Hostile-Neural-Networks#59

--- fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/mixin/object/builder/BlockEntityTypeMixin.java
+++ fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/mixin/object/builder/BlockEntityTypeMixin.java
@@ -16,11 +16,12 @@
 
 package net.fabricmc.fabric.mixin.object.builder;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
-
+import com.google.common.collect.ImmutableSet;
 import com.mojang.datafixers.types.Type;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -28,10 +29,10 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityType;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Mixin(BlockEntityType.class)
 public class BlockEntityTypeMixin<T extends BlockEntity> implements FabricBlockEntityType {
@@ -42,7 +43,9 @@ public class BlockEntityTypeMixin<T extends BlockEntity> implements FabricBlockE
 
 	@Inject(method = "<init>", at = @At("RETURN"))
 	private void mutableBlocks(BlockEntityType.BlockEntitySupplier<? extends T> factory, Set<Block> blocks, Type<?> type, CallbackInfo ci) {
-		this.validBlocks = new HashSet<>(this.validBlocks);
+		if (this.validBlocks instanceof ImmutableSet<Block>) {
+			this.validBlocks = new HashSet<>(this.validBlocks);
+		}
 	}
 
 	@Override
